0fbe565602f1f441fd60f7f38eb5184a3f6091e4
Bisimwa Grace <bisimwagrace596@gmail.com>
2024-08-18 00:08:39 +0200

    NavLinks added to Sidebar

--- app/dashboard/components/Sidebar.tsx
+++ app/dashboard/components/Sidebar.tsx
@@ -1,8 +1,14 @@
+"use client";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import { NavLinks } from "..";
+import { usePathname } from "next/navigation";
 
 export default function Sidebar() {
+  const pathname = usePathname();
+  const activeLink =
+    "/dashboard" + (pathname.split("/")[2] ? "/" + pathname.split("/")[2] : "");
   return (
     <>
       {/* Logo and the Toggle Icon */}
@@ -22,6 +28,24 @@ export default function Sidebar() {
       </div>
 
       {/* NavLinks */}
+      <div className='mt-5'>
+        {NavLinks.map(({ id, name, link, icon: Icon }) => (
+          <Link
+            href={link}
+            key={id}
+            className={`transition-all links flex items-center gap-5 py-5 mr-4 font-normal hover:font-semibold ${
+              activeLink === link && "activeLink"
+            }`}
+          >
+            <Icon
+              className={`ml-[48px] w-5 h-5 ${
+                activeLink === link && "font-semibold text-3xl"
+              }`}
+            />
+            <span className='hidden md:block'>{name}</span>
+          </Link>
+        ))}
+      </div>
 
       {/* Toggle Actions */}
 
