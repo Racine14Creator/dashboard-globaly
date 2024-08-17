21359c02e3e2f8967615adc9385045e575d36413
Bisimwa Grace <bisimwagrace596@gmail.com>
2024-08-17 23:30:26 +0200

    Logo

--- app/dashboard/components/Sidebar.tsx
+++ app/dashboard/components/Sidebar.tsx
@@ -1,8 +1,25 @@
+import { ChevronLeft, ChevronRight } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Sidebar() {
   return (
     <>
-      <h3>Sidebar</h3>
       {/* Logo and the Toggle Icon */}
+
+      <div className='wrapper mt-[21px]'>
+        <div className='hidden md:flex md:w-[196px] md:justify-between items-center gap-10'>
+          <Link href={"/dashboard"}>
+            <Image
+              alt='Globaly Logo'
+              src={`/assets/Logo-Globaly.svg`}
+              width={196}
+              height={84}
+            />
+          </Link>
+          <ChevronLeft className='w-[29px] h-[29px] cursor-pointer font-bold' />
+        </div>
+      </div>
 
       {/* NavLinks */}
 
